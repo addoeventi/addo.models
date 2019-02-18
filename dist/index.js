24d1bc3ccebae7bdfc11da898f43b9d41d54286a
Administrator <developer@addoeventi.it>
2019-02-18 18:13:12 +0100

    booking: any => Booking

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+// COMMON
+__export(require("./models/user.model"));
+__export(require("./models/event.model"));
+// RESERVATIONS
+__export(require("./models/booking.model"));
+__export(require("./models/group.model"));
+__export(require("./models/event-group.model"));
+__export(require("./models/reservation-type.enum"));
+__export(require("./models/reservation.model"));
+// WAREHOUSE
+__export(require("./models/discharge.model"));
+__export(require("./models/shopping-list.model"));
+__export(require("./models/warehouse.model"));
+__export(require("./models/product.model"));
