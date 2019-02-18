5bb1b614f6b0985da6ab934fb151cbbb872927b5
Administrator <developer@addoeventi.it>
2019-02-18 17:11:25 +0100

    model->index

---
+++ index.ts
@@ -0,0 +1,17 @@
+// COMMON
+export * from "./models/user.model";
+export * from "./models/event.model";
+
+// RESERVATIONS
+export * from "./models/booking.model";
+export * from "./models/group.model";
+export * from "./models/event-group.model";
+export * from "./models/reservation-type.enum";
+export * from "./models/reservation.model";
+
+// WAREHOUSE
+export * from "./models/discharge.model";
+export * from "./models/shopping-list.model";
+export * from "./models/warehouse.model";
+export * from "./models/product.model";
+ 
