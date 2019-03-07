284cd473e9776306e8969aa89eeba3ec85e06642
Simone di Sante <developer@addoeventi.it>
2019-03-07 10:26:07 +0100

    add service=>booking

--- models/booking.model.ts
+++ models/booking.model.ts
@@ -1,11 +1,23 @@
 import { EventGroup } from "./event-group.model";
 
 export class Booking {
-    public administrators: string[] = [];
-    public groups: EventGroup[] = [];
-    public reservations: string[] = [];
-    public status: string;
-    public packages: any;
-    public limits: any;
-    public info: any;
+  public administrators: string[] = [];
+  public groups: EventGroup[] = [];
+  public reservations: string[] = [];
+  public status: string;
+  public packages: any;
+  public info: any;
+  public services: { [name: string]: Service };
 }
+
+declare type Service = {
+  total: number;
+  packages: Array<{
+    name: string;
+    price: number;
+    description: string;
+    info: any;
+    avaiability: number;
+  }>;
+};
+
